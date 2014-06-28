a5f69b063ff5424e9089b83910bd7a39e9cc5f85
Boris Kaganov <boris.kaganov@nodesquad.com>
2014-06-29 00:22:37 +0400

    initial

---
+++ clazz.js
@@ -0,0 +1,73 @@
+//Inheritance utility class. Now supports just single inheritance
+var Clazz = function() {
+    function constructionMethods(extendFunc) {
+        return {
+            baseCallDirect: function(baseClass, args) {
+                //create private scope of parent class and connect it to constructed object
+                extendFunc(this, typeof(baseClass) !== 'function' ? baseClass : baseClass.apply(this, args));
+                this.base = extendFunc({}, this); //create parent fields table and save link to it
+            },
+
+            //we need __baseClasses, baseCall, baseApply just to not be obliged specifying base class name in 2 places
+            //(inherit call and constructor call). We can still call base constructor directly with baseCallDirect
+
+            //Assumption: caller didn't modify 'thiz' (for example, setting fields) before call (needed for parent fields table)
+            //Has variable number of arguments, all passed to base constructor
+            baseCall: function() {
+                var baseClassesBackup = this.__baseClasses;
+                this.__baseClasses = this.__baseClasses.slice();
+                //remove base class from __baseClasses. make it equal to own chain of inheritance of base class
+                var baseClass = this.__baseClasses.pop();
+                this.baseCallDirect(baseClass, arguments);
+                this.__baseClasses = baseClassesBackup;
+            },
+
+            baseApply: function(args) {
+                this.baseCall.apply(this, args);
+            }
+        }
+    }
+    return {
+        //simplest copying of fields.
+        //Use it if no need for own properties check or other complex stuff, overwise use things like jQuery.extend
+        extend: function(source, target) {
+            for (var k in target) {
+                source[k] = target[k];
+            }
+            return source;
+        },
+        //base, clazz: constructors of classes, whether function or object.
+        //(object is used if no need for construction code and private fields)
+        //Assumption: if base and class are functions they return nothing (they use .extend function for defining fields)
+        //Assumption: base constructor supports calling without parameters (prototype construction mode)
+        //extendFunc - function for merging prototypes, optional. Used if simplest extend isn't enough
+        inherit: function(base, clazz, extendFunc) {
+            extendFunc = extendFunc || Clazz.extend;
+            var constructor;
+            if (typeof(clazz) !== 'function') {
+                constructor = function() {
+                    this.baseApply(arguments);
+                };
+                constructor.prototype = clazz;
+            } else {
+                constructor = clazz;
+                constructor.prototype = {};
+            }
+            var baseProto = base;
+            if (typeof(base) !== 'function') {
+                baseProto = function() {};
+                baseProto.prototype = base;
+            }
+            Clazz.extend(constructor.prototype, new baseProto());
+            constructor.prototype.constructor = constructor;
+            constructor.prototype.__baseClasses = constructor.prototype.__baseClasses || []; //chain of base classes
+            constructor.prototype.__baseClasses.push(baseProto); // (for tracking currently constructed ancestor)
+            Clazz.extend(constructor.prototype,  constructionMethods(extendFunc));
+            return constructor;
+        }
+    }
+}();
+
+if (module) {
+    module.exports = Clazz.Clazz = Clazz;
+}
---
+++ test.js
@@ -0,0 +1,146 @@
+var Clazz = require('./');
+var test = require('tap').test;
+
+test('inherit from function constructor', function (t) {
+
+    var r = function () {
+        Clazz.extend(this, {
+            getP:function () {
+                return p;
+            },
+            getZ:function () {
+                return this.zz;
+            },
+            setP:function (i) {
+                this.zz = i + 10;
+                p = i
+            }
+        });
+        if (!arguments.length) {
+            return; //prototype creation mode
+        }
+        var p = 0;
+        this.zz = 0;
+    };
+
+    var n = Clazz.inherit(r, function () {
+        this.baseApply(arguments);
+        Clazz.extend(this, {})
+    });
+    var j = Clazz.inherit(r, function () {
+        this.baseApply(arguments);
+        Clazz.extend(this, {})
+    });
+
+    var u = Clazz.inherit(n, function () {
+        this.baseApply(arguments);
+        Clazz.extend(this, {
+            setP:function (i) {
+                this.base.setP.call(this, i + 1);
+            }
+        })
+    });
+
+    var a = new r();
+    a.setP(1);
+    var b = new r();
+    b.setP(2);
+
+
+    var z = new j();
+    z.setP(3);
+    var q = new j();
+    q.setP(4);
+    var m = new n();
+    m.setP(5);
+    var h = new n();
+    h.setP(6);
+
+    var o = new u();
+    o.setP(7);
+    t.equal(a.getP(),1);
+    t.equal(b.getP(),2);
+    t.equal(z.getP(),3);
+    t.equal(q.getP(),4);
+    t.equal(m.getP(),5);
+    t.equal(h.getP(),6);
+    t.equal(o.getP(),8);
+
+    t.equal(a.getZ(),11);
+    t.equal(b.getZ(),12);
+    t.equal(z.getZ(),13);
+    t.equal(q.getZ(),14);
+    t.equal(m.getZ(),15);
+    t.equal(h.getZ(),16);
+    t.equal(o.getZ(),18);
+    t.end();
+});
+
+test('inherit from literal object constructor', function (t) {
+
+    var r = function () {
+        Clazz.extend(this, {
+            getP:function () {
+                return p;
+            },
+            getZ:function () {
+                return this.zz;
+            },
+            setP:function (i) {
+                this.zz = i + 10;
+                p = i
+            }
+        });
+        if (!arguments.length) {
+            return; //prototype creation mode
+        }
+        var p = 0;
+        this.zz = 0;
+    };
+
+    var n = Clazz.inherit(r, {});
+    var j = Clazz.inherit(r, {});
+
+    var u = Clazz.inherit(n, function () {
+        this.baseApply(arguments);
+        Clazz.extend(this, {
+            setP:function (i) {
+                this.base.setP.call(this, i + 1);
+            }
+        })
+    });
+
+    var a = new r();
+    a.setP(1);
+    var b = new r();
+    b.setP(2);
+
+
+    var z = new j();
+    z.setP(3);
+    var q = new j();
+    q.setP(4);
+    var m = new n();
+    m.setP(5);
+    var h = new n();
+    h.setP(6);
+
+    var o = new u();
+    o.setP(7);
+    t.equal(a.getP(),1);
+    t.equal(b.getP(),2);
+    t.equal(z.getP(),3);
+    t.equal(q.getP(),4);
+    t.equal(m.getP(),5);
+    t.equal(h.getP(),6);
+    t.equal(o.getP(),8);
+
+    t.equal(a.getZ(),11);
+    t.equal(b.getZ(),12);
+    t.equal(z.getZ(),13);
+    t.equal(q.getZ(),14);
+    t.equal(m.getZ(),15);
+    t.equal(h.getZ(),16);
+    t.equal(o.getZ(),18);
+    t.end();
+});
